cbc9a3ad8b3236dc7f03a4af936db695baed2b75
JustMeGaaRa <gaarasmail@gmail.com>
2019-05-21 18:37:27 +0300

    initial project with 2 endpoints for getting and creating the documents

---
+++ assessment-profiles-api/index.ts
@@ -0,0 +1,17 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    switch (req.method) {
+        case "POST":
+            context.bindings.assessmentProfile = JSON.stringify(req.body);
+            break;
+        case "DELETE":
+            break;
+        }
+    
+    context.res = {
+        body: context.bindings.documents
+    };
+};
+
+export default httpTrigger;
